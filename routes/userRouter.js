7a6d48967549a619a10315c7a31345d0001e8c99
Gauravsunil <32197840+Gauravsunil@users.noreply.github.com>
2020-08-18 14:58:33 +0530

    Updated Algorithm

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -6,31 +6,70 @@ userRouter.get('/string/:strng',(req,res,next)=>{
 
   console.log(req.params.strng);
     var str=req.params.strng;
-    let decoded = {
-      a: '2', b: 'o', c: 'p',
-      d: 'q', e: 'r', f: 's',
-      g: 't', h: 'u', i: 'v',
-      j: '5', k: 'x', l: 'y',
-      m: '9', n: 'n', o: 'b',
-      p: 'c', q: 'd', r: 'e',
-      s: 'f', t: 'g', u: 'h',
-      v: 'i', w: 'w', x: 'k',
-      y: 'l', z: 'z', 2:'a',
-      5: 'j', 9: 'm',   
+    var fromBase=26;
+    var toBase=64;
+    const DIGITS ="0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ+/";
+
+    const add = (x, y, base) => {
+        let z = [];
+        const n = Math.max(x.length, y.length);
+        let carry = 0;
+        let i = 0;
+        while (i < n || carry) {
+            const xi = i < x.length ? x[i] : 0;
+            const yi = i < y.length ? y[i] : 0;
+            const zi = carry + xi + yi;
+            z.push(zi % base);
+            carry = Math.floor(zi / base);
+            i++;
+        }
+        return z;
     }
-    
-    //convert the string to lowercase
-    str = str.toLowerCase();
-    
-    //decipher the code
-    let decipher = '';
-    for(let i = 0 ; i < str.length; i++){
-      decipher += decoded[str[i]];
+
+    const multiplyByNumber = (num, x, base) => {
+        if (num < 0) return null;
+        if (num == 0) return [];
+
+        let result = [];
+        let power = x;
+        while (true) {
+            num & 1 && (result = add(result, power, base));
+            num = num >> 1;
+            if (num === 0) break;
+            power = add(power, power, base);
+        }
+
+        return result;
     }
-    
-    //return the output
-    res.send(decipher);
+
+    const parseToDigitsArray = (str, base) => {
+        const digits = str.split('');
+        let arr = [];
+        for (let i = digits.length - 1; i >= 0; i--) {
+            const n = DIGITS.indexOf(digits[i])
+            if (n == -1) return null;
+            arr.push(n);
+        }
+        return arr;
+    }
+
+    const digits = parseToDigitsArray(str, fromBase);
+    if (digits === null) return null;
+
+    let outArray = [];
+    let power = [1];
+    for (let i = 0; i < digits.length; i++) {
+        digits[i] && (outArray = add(outArray, multiplyByNumber(digits[i], power, toBase), toBase));
+        power = multiplyByNumber(fromBase, power, toBase);
+    }
+
+    let out = '';
+    for (let i = outArray.length - 1; i >= 0; i--)
+        out += DIGITS[outArray[i]];
+
+    res.send(out);
     res.end();
+
 })
 
 module.exports=userRouter;
